4d015ffbed88690139e753e21f689b1f1530f6de
andytm99 <andytm99@yahoo.com>
2022-01-03 19:42:57 +0200

    Main screen connected to the register screen. Buttons functionalities.

--- app/src/main/java/com/example/stayfit/MainActivity.java
+++ app/src/main/java/com/example/stayfit/MainActivity.java
@@ -2,13 +2,31 @@ package com.example.stayfit;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import org.w3c.dom.Text;
 
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+        private TextView register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        register=(TextView) findViewById(R.id.register);
+        register.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.register:
+                startActivity(new Intent(this,RegisterUser.class ));
+                break;
+        }
     }
 }
