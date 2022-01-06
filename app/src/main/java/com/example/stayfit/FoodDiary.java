efc84a444a98660e5d8a765475393e900ebc98c2
andytm99 <andytm99@yahoo.com>
2022-01-06 21:02:51 +0200

    added category for food diary

--- app/src/main/java/com/example/stayfit/FoodDiary.java
+++ app/src/main/java/com/example/stayfit/FoodDiary.java
@@ -2,17 +2,18 @@ package com.example.stayfit;
 
 public class FoodDiary {
     private String email,day,month,year;
-    private String foodName,brandName,calories,carbs,fats,proteins,barcode;
+    private String foodName,category,brandName,calories,carbs,fats,proteins,barcode;
 
     public FoodDiary(){
 
     }
-    public FoodDiary(String email,String day, String month,String year,String foodName,String brandName,String calories,String carbs,String fats, String proteins,String barcode){
+    public FoodDiary(String email,String day, String month,String year,String foodName,String category,String brandName,String calories,String carbs,String fats, String proteins,String barcode){
         this.email=email;
         this.day=day;
         this.month=month;
         this.year=year;
         this.foodName=foodName;
+        this.category=category;
         this.brandName=brandName;
         this.calories=calories;
         this.carbs=carbs;
